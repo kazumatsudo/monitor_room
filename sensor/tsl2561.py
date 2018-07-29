6ace45103aa0f17cac56d837a4c4398ebc5bec19
Kazuma Matsudo <kazuma.matsudo@icloud.com>
2018-07-29 14:08:55 +0900

    TSL2561 コメントを添える

--- sensor/tsl2561.py
+++ sensor/tsl2561.py
@@ -1,8 +1,20 @@
 class Tsl2561:
+    """
+    TSL2561 - 照度センサーモジュール
+    http://akizukidenshi.com/catalog/g/gM-08219/
+
+    上記センサーの値を取得するクラス
+    """
     def __init__(self, bus):
         self.bus = bus
         self.i2c_address_tsl2561 = 0x39
         bus.write_byte_data(self.i2c_address_tsl2561, 0x80, 0x03)
 
     def read_data(self):
+        """
+        センサーから取得した値を返す
+
+        :return: float
+            照度(lux)
+        """
         return self.bus.read_word_data(self.i2c_address_tsl2561, 0xAC)
