3bc66edec5ddbcd415c073704fa7eacc70426183
Kazuma Matsudo <kazuma.matsudo@icloud.com>
2018-07-29 14:06:53 +0900

    MH-Z19 コメントを添える

--- sensor/mh_z19.py
+++ sensor/mh_z19.py
@@ -2,6 +2,12 @@ import serial
 
 
 class MhZ19:
+    """
+    MH-Z19 - CO2二酸化炭素センサーモジュール
+    https://www.amazon.co.jp/dp/B01MZI8O5E
+
+    上記センサーの値を取得するクラス
+    """
     def __init__(self):
         self.ser = serial.Serial('/dev/ttyS0',
                             baudrate=9600,
@@ -12,5 +18,11 @@ class MhZ19:
         self.ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
 
     def read_data(self):
+        """
+        センサーから取得した値を返す
+
+        :return: float
+            二酸化炭素濃度(ppm)
+        """
         result = self.ser.read(9)
         return result[2] * 256 + result[3]
