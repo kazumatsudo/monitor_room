97f40b65ca66611c524fccbec451715346c50c12
Kazuma Matsudo <kazuma.matsudo@icloud.com>
2018-07-29 13:50:58 +0900

    util コメントを添える

--- util/util.py
+++ util/util.py
@@ -1,10 +1,17 @@
-from smbus2 import SMBus
-from util.constants import BUS_NUMBER
+"""
+共通関数
+"""
 
 
 def calculate_discomfort(humidity, temperature):
+    """
+    不快指数を計算する
+
+    :param humidity: int
+        湿度 (％)
+    :param temperature: int
+        温度 (℃)
+    :return: int
+        不快指数 (％)
+    """
     return 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
-
-
-def get_bus():
-    return SMBus(BUS_NUMBER)
